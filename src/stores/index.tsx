03c1de9c9d19e7159b150aca618aa80ee86706bb
wangdelei <jimmy.wang@gaodun.com>
2021-07-22 20:51:09 +0800

    案例调整

--- src/stores/index.tsx
+++ src/stores/index.tsx
@@ -3,19 +3,19 @@
  * @Author: wangdelei
  * @Date: 2021-07-20 10:33:35
  * @LastEditors: wangdelei
- * @LastEditTime: 2021-07-22 20:43:02
+ * @LastEditTime: 2021-07-22 20:50:37
  */
 import { createStore, applyMiddleware  } from 'redux';
 import thunkMiddleware from 'redux-thunk'; // 允许我们 dispatch() 函数
 import storeInit from './reducers';
-import {
-    addCount,
-    minusCount,
-    getDataList
-} from './actions';
+// import {
+//     addCount,
+//     minusCount,
+//     getDataList
+// } from './actions';
 
 export let store = createStore(storeInit, applyMiddleware(thunkMiddleware));
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
+// const unsubscribe = store.subscribe(() => console.log(store.getState()));
 // store.dispatch(getDataList());
 // store.dispatch(addCount(1));
 // store.dispatch(minusCount(1));
