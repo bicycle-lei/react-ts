21e7cb7cbd7c06c95aea84cda4f7849ed7ca3239
wangdelei <jimmy.wang@gaodun.com>
2021-07-23 11:24:00 +0800

    优化

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,17 +3,19 @@
  * @Author: wangdelei
  * @Date: 2021-07-13 14:47:58
  * @LastEditors: wangdelei
- * @LastEditTime: 2021-07-23 11:18:44
+ * @LastEditTime: 2021-07-23 11:23:22
  */
 // import { Link } from 'react-router-dom';
 // import { useParams } from 'react-router-dom'; // 获取路由参数
+import { useState } from 'react';
 import { store } from '@/stores/index';
 import { Button } from 'antd';
 function About() {
     // const { id } = useParams();
+    const [count, setCount] = useState<Number>(store.getState().count);
     return (
         <div>
-            <div>关于页</div>
+            <div>我是redux存储的count：{count}</div>
             <Button
                 type="primary"
                 onClick={() => {
@@ -21,6 +23,7 @@ function About() {
                         type: 'ADD_COUNT',
                         count: 1,
                     });
+                    setCount(store.getState().count);
                 }}
             >
                 conut++
